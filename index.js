bb0177311ce85cd47df0a0ea6a197859e3ab555c
Bruno Marques <bruno@bmarques.net>
2023-03-29 02:02:05 +0100

    Rewrote to Helix v6 support

--- index.js
+++ index.js
@@ -4,49 +4,41 @@ import fetch from 'node-fetch';
 import dotenv from 'dotenv';
 import lodash from 'lodash';
 import { RefreshingAuthProvider } from '@twurple/auth';
-import { ChatClient } from '@twurple/chat';
+import { ApiClient } from '@twurple/api';
 
 dotenv.config();
-const TWITCH_BOTS_URL = 'https://api.twitchinsights.net/v1/bots/online';
+const TWITCH_BOTS_URL = 'https://api.twitchinsights.net/v1/bots/all';
 const BAN_REASON = 'Wir fahren, fahren, fahren auf der AutoBAN! Caught by the sweeper...';
+const WHITELIST = ['streamlabs', 'streamelements', 'nightbot', 'wizebot']; // TODO a proper replacement
 
-async function run(channel, clientId, clientSecret) {
-    const data = JSON.parse(await fs.readFile('./tokens.json', 'utf-8'));
-    const auth = new RefreshingAuthProvider(
+async function run(userId, channelName, clientId, clientSecret) {
+    const data = JSON.parse(await fs.readFile(`./tokens.${userId}.json`, 'utf-8'));
+    const authProvider = new RefreshingAuthProvider(
         {
             clientId,
             clientSecret,
-            onRefresh: newData => fs.writeFile('./tokens.json', JSON.stringify(newData), 'utf-8')
-        },
-        data
+            onRefresh: (userId, newData) => fs.writeFile(`./tokens.${userId}.json`, JSON.stringify(newData), 'utf-8')
+        }
     );
-    const client = new ChatClient({
-        authProvider: auth,
-        channels: [channel],
-        requestMembershipEvents: true
-    });
+    const client = new ApiClient({ authProvider });
+    authProvider.addUser(userId, data);
 
-    let mods;
     const botsResponse = await fetch(TWITCH_BOTS_URL);
     const botsRaw = await botsResponse.json();
     const bots = lodash.unzip(botsRaw.bots)[0];
-    client.onJoin(async (channel, user) => {
-        if (user === client.currentNick) {
-            mods = await client.getMods(channel);
-        } else if (bots.indexOf(user) >= 0) {
-            if (mods.indexOf(user) >= 0) {
-                console.log("Found bot %s on chat - but it's a mod", user);
+
+    const channel = await client.users.getUserByName(channelName);
+    const chatters = await client.chat.getChatters(channel.id, userId);
+    chatters.data.forEach(async chatter => { // TODO handle pagination
+        if (bots.indexOf(chatter.userName) >= 0) {
+            if (WHITELIST.indexOf(chatter.userName) >= 0) {
+                console.log("Found bot %s on chat - but it's whitelisted", chatter.userDisplayName);
             } else {
-                console.log("Found bot %s on chat - BAN!", user);
-                client.ban(channel, user, BAN_REASON);
+                console.log("Found bot %s on chat - BAN!", chatter.userDisplayName);
+                client.moderation.banUser(channel.id, userId, { user: chatter.userId, reason: BAN_REASON });
             }
         }
     });
-
-    await client.connect();
-    setTimeout(() => {
-        client.quit();
-    }, 60000);
 }
 
 const channel = process.argv[2];
@@ -54,4 +46,4 @@ if (channel === undefined) {
     console.error("Need a channel name!");
     process.exit(1);
 }
-run(channel, process.env.CLIENT_ID, process.env.CLIENT_SECRET);
+run(process.env.USER_ID, channel, process.env.CLIENT_ID, process.env.CLIENT_SECRET);
